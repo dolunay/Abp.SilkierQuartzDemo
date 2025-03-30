beba3b8950c87c478aebdc10f05c51a581f4e180
Dolunay <admin@gelistirici.com>
2025-03-30 23:31:36 +0300

    Upgrade to ABP 9.1.1 from 9.0 and readme file added with screen shots.

--- src/Abp.SilkierQuartzDemo.EntityFrameworkCore/Migrations/20250330194520_Upgrade_Abp9_1_1.Designer.cs
+++ src/Abp.SilkierQuartzDemo.EntityFrameworkCore/Migrations/20250330194520_Upgrade_Abp9_1_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Abp.SilkierQuartzDemo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Abp.SilkierQuartzDemo.Migrations
 {
     [DbContext(typeof(SilkierQuartzDemoDbContext))]
-    partial class SilkierQuartzDemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250330194520_Upgrade_Abp9_1_1")]
+    partial class Upgrade_Abp9_1_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
